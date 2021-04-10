8f0f40329f4b60eb9ff24b36a79a2c83a2129a45
Daniel Szlicht <daniel.szlicht25@gmail.com>
2021-04-10 09:51:46 +0200

    sda edu task 7

--- Java Podstawy/Zadania/task07/src/Task.java
+++ Java Podstawy/Zadania/task07/src/Task.java
@@ -4,6 +4,14 @@ public class Task {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
+        int power = 1;
+        // power = power *2;
+
+        while (power<=n){
+            System.out.println(power);
+            power *= 2;
+        }
+
 
         // Tu zaimplementuj rozwiązanie
     }
--- Java podstawy - programowanie/Zadania/task05/src/Task.java
+++ Java podstawy - programowanie/Zadania/task05/src/Task.java
@@ -3,7 +3,32 @@ import java.util.Scanner;
 public class Task {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-// your code
+        float number = scanner.nextFloat();
+        System.out.println( roundFloor(number));
+        System.out.println( round(number));
+        System.out.println( roundUp(number));
     }
+
+   static int roundFloor(double value) {
+        return (int) value;
+    }
+
+    static int round(double value) {
+        return (int)Math.round(value);
+    }
+
+    static int roundUp(double value){
+        return (int) Math.ceil(value);
+    }
+
+  /*  static int roundFloor1(double value) {
+        return (int) value;
+    }
+
+
+    static int roundUp1(double value){
+        return  roundFloor1(value) + 1;
+    }*/
+
+
 }
